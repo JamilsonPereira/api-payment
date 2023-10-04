013882d5b5bb78c9c887c8fddd945cac59163333
J164716 <jamilson.pereira@sulamerica.com.br>
2023-10-04 04:09:36 -0300

    Configurando kafka via docker

--- payment/src/main/java/br/itau/payment/service/PaymentService.java
+++ payment/src/main/java/br/itau/payment/service/PaymentService.java
@@ -1,9 +1,11 @@
 package br.itau.payment.service;
 
+import br.itau.payment.domain.Payment;
 import br.itau.payment.dto.PaymentRequest;
 import br.itau.payment.dto.PaymentResponse;
 import br.itau.payment.producer.KafkaProducer;
 import br.itau.payment.repository.PaymentRepository;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +27,17 @@ public class PaymentService {
 
     private static final Logger logger = LoggerFactory.getLogger(PaymentService.class);
 
-    public void createPay(PaymentRequest pay){
-        paymentRepository.save(mapperService.mapperDtoToDomain(pay));
-        kafkaProducer.send("payment", "key", pay.toString());
+    public void createPay(PaymentRequest pay) {
+        logger.info("Save Payment");
+        Payment result = paymentRepository.save(mapperService.mapperDtoToDomain(pay));
+        Gson gson = new Gson();
+        String message = gson.toJson(result);
+        logger.info("Send message for topic kafka");
+        kafkaProducer.send("notifications.payment.request", null, message);
     }
 
-    public List<PaymentResponse> findAllPayment(){
-       return mapperService.mapperListDomainToListDto(paymentRepository.findAll());
+    public List<PaymentResponse> findAllPayment() {
+        return mapperService.mapperListDomainToListDto(paymentRepository.findAll());
     }
 
 
